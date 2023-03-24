b051d0f16e16ce79285e7150e3a27e98a82628ee
Tsoi Wai Mun <waimun.tsoi@student-cs.fr>
2023-03-24 22:44:52 +0100

    reordering

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt-get install python3.9 -y
 
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
-
+sudo update-alternatives --config python3 1
 sudo update-alternatives  --set python /usr/bin/python3.9
 sudo apt install python3.9-distutils -y
 
