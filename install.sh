54a8ce76b7193c6692a30326d5b946f805409588
Tsoi Wai Mun <waimun.tsoi@student-cs.fr>
2023-03-25 00:57:40 +0100

    another try

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # !/bin/sh
 
 sudo apt update 
-sudo apt install software-properties-common 
+sudo apt install software-properties-common -y 
 
 
 sudo add-apt-repository ppa:deadsnakes/ppa -y
@@ -10,9 +10,9 @@ apt list | grep python3.9
 sudo apt-get install python3.9 -y
 
 
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
-sudo update-alternatives --config python3 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --config python3 2
 sudo update-alternatives  --set python /usr/bin/python3.9
 sudo apt install python3.9-distutils -y
 
