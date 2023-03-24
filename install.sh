5d7cea3a9ca4404b2fc1d0164b5e7ee728c8b9c6
Tsoi Wai Mun <waimun.tsoi@student-cs.fr>
2023-03-24 22:26:07 +0100

    please work git add .git add .

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ sudo apt-get update -y
 apt list | grep python3.9
 sudo apt-get install python3.9 -y
 
-python3 -V
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
+sudo update-alternatives  --set python /usr/bin/python3.9
+sudo apt install python3.9-distutils -y
 
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
-sudo update-alternatives --config python3 2
-sudo apt install python3.9-distutils
+python3 -V
 
 sudo apt install python3-pip -y
 sudo apt-get install -y --no-install-recommends openmpi-bin
