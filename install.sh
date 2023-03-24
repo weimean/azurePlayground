e71af3e8f0d2d659042676c44e7978813b15d05f
Tsoi Wai Mun <waimun.tsoi@student-cs.fr>
2023-03-24 21:42:39 +0100

    new update

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 # !/bin/sh
 
+sudo apt update 
+sudo apt install software-properties-common 
+
+
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update -y
 apt list | grep python3.9
 sudo apt-get install python3.9 -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1 -y
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2 -y
-sudo update-alternatives --config python3 -y
+
 python3 -V
+
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+sudo update-alternatives --config python3 2
+
 
 sudo apt install python3-pip -y
 sudo apt-get install -y --no-install-recommends openmpi-bin
