23b85c96ae5129e71d9962babf6af19207ab3570
Tsoi Wai Mun <waimun.tsoi@student-cs.fr>
2023-03-24 20:15:56 +0100

    update way to install python3.9

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 # !/bin/sh
 
-sudo add-apt-repository ppa:deadsnakes/ppa 
-sudo apt update -y
-sudo apt install python3.9
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update -y
+apt list | grep python3.9
+sudo apt-get install python3.9
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+sudo update-alternatives --config python3
+python3 -V
+
 sudo apt install python3-pip -y
 sudo apt-get install -y --no-install-recommends openmpi-bin
 sudo apt-get install -y libopenmpi-dev
