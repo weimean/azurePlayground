d3e3b3e5c637d32518d92db6dc292a3dd07e3947
Tsoi Wai Mun <waimun.tsoi@student-cs.fr>
2023-03-25 03:08:04 +0100

    trying py3.7

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 sudo apt update 
 sudo apt install software-properties-common -y 
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.7 -y
 sudo apt install python3-pip -y
 sudo apt-get install -y --no-install-recommends openmpi-bin
 sudo apt-get install -y libopenmpi-dev
