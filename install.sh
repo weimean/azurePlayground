c70c026027ceb62c569b2a233ef477f598bcc939
Tsoi Wai Mun <waimun.tsoi@student-cs.fr>
2023-03-25 01:32:29 +0100

    using python3.8 instead

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ sudo apt install software-properties-common -y
 
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update -y
-apt list | grep python3.9
-sudo apt-get install python3.9 -y
+apt list | grep python3.8
+sudo apt-get install python3.8 -y
 
 
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 sudo update-alternatives --config python3 2
-sudo update-alternatives  --set python /usr/bin/python3.9
-sudo apt install python3.9-distutils -y
+sudo update-alternatives  --set python /usr/bin/python3.8
+sudo apt install python3.8-distutils -y
 
 python3 -V
 
