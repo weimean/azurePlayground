575dedb800d184176905fe7a3970c20743ef92e1
Tsoi Wai Mun <waimun.tsoi@student-cs.fr>
2023-03-25 04:20:17 +0100

    trying to fix pillow problem

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt install python3-pip -y
 # sudo update-alternatives  --set python3 /usr/bin/python3.7
 
 sudo apt-get install python3-dev python3-setuptools
-sudo apt-get install libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk libharfbuzz-dev libfribidi-dev libxcb1-dev
+sudo apt-get install libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk libharfbuzz-dev libfribidi-dev libxcb1-dev zlib1g-dev
 
 
 sudo apt-get install -y --no-install-recommends openmpi-bin
